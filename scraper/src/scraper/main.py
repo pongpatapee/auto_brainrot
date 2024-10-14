63038e15fcaef51e42acc89f7d16a4cbd9d42e4e
pongpatapee <pongpatapee1021@gmail.com>
2024-10-14 08:02:52 -0400

    feat: init scraper, can scrape title and content from post

--- scraper/src/scraper/main.py
+++ scraper/src/scraper/main.py
@@ -1,1 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
+
+
+# Making a GET request
+r = requests.get(
+    "https://www.reddit.com/r/tifu/comments/1g2trss/tifu_by_using_the_bathroom_at_my_dates_house/"
+)
+
+# Parsing the HTML
+soup = BeautifulSoup(r.text, "html.parser")
+# s = soup.find("h1", class_="title")
+title_tag = soup.find("h1")
+print(title_tag.text)
+
+paragraphs = (
+    soup.find("div", class_="text-neutral-content")
+    .find("div")
+    .find("div")
+    .find_all("p")
+)
+
+for p in paragraphs:
+    print(p)
+
+print(len(paragraphs))
+
+# post_content = content_tag.text if content_tag else "N/A"
+# print(post_content)
+#
